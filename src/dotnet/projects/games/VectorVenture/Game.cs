92bf9171f9e1de2a26b2e89314568445e29ec520
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-01-10 23:25:07 -0500

    Start VectorVenture game

---
+++ src/dotnet/projects/games/VectorVenture/Game.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Lucas Girouard-Stranks (https://github.com/lithiumtoast). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the Git repository root directory (https://github.com/lithiumtoast/sokol-cs/) for full license information.
+
+using Katabasis;
+
+namespace VectorVenture
+{
+	public class VectorVentureGame : Game
+	{
+	}
+}
