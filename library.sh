0d94a5e3e6068e61a66fdd112f1af0a83d8e31ea
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-09-11 10:53:29 -0400

    Update `library.sh`

--- library.sh
+++ library.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-FNA_LIBS_DIR=$DIR/fnalibs
+LIBS_DIR=$DIR/lib/
+mkdir -p $LIBS_DIR
 
 if [[ ! -z "$1" ]]; then
     TARGET_BUILD_PLATFORM="$1"
@@ -45,10 +46,10 @@ set_target_build_platform
 # Build SDL
 echo "Building SDL from source..."
 $DIR/ext/sdl-cs/library.sh
-mv -v $DIR/ext/sdl-cs/lib/* $DIR/lib/
+mv -v $DIR/ext/sdl-cs/lib/* $LIBS_DIR
 echo "Building SDL from source finished!"
 
-# FNA3D
+# Build FNA3D
 echo "Building FNA3D from source..."
 if [[ "$TARGET_BUILD_PLATFORM" == "linux" ]]; then
     SDL_LIBRARY_FILE_PATH="$DIR/ext/sdl-cs/lib/libSDL2.so"
@@ -58,55 +59,58 @@ elif [[ "$TARGET_BUILD_PLATFORM" == "microsoft" ]]; then
     SDL_LIBRARY_FILE_PATH="$DIR/ext/sdl-cs/lib/SDL2.dll"
 fi
 $DIR/ext/FNA3D-cs/library.sh $TARGET_BUILD_PLATFORM $SDL_LIBRARY_FILE_PATH $DIR/ext/sdl-cs/ext/SDL/include
-mv -v $DIR/ext/FNA3D-cs/lib/* $DIR/lib/
+mv -v $DIR/ext/FNA3D-cs/lib/* $LIBS_DIR
 echo "Building FNA3D from source finished!"
 
 # Build cimgui
 echo "Building imgui from source..."
 $DIR/ext/imgui-cs/library.sh
-mv -v $DIR/ext/imgui-cs/lib/* $DIR/lib/
+mv -v $DIR/ext/imgui-cs/lib/* $LIBS_DIR
 echo "Building imgui from source finished!"
 
-# Downloading
-echo "Downloading latest FNA libraries ..."
-curl -L https://github.com/deccer/FNA-libs/archive/refs/heads/main.zip > "$DIR/fnalibs.zip"
-if [ $? -eq 0 ]; then
-    echo "Finished downloading!"
-else
-    >&2 echo "ERROR: Unable to download successfully."
-    exit 1
-fi
+function download_fna_libraries() {
+    echo "Downloading latest FNA libraries ..."
+    FNA_LIBS_DIR=$DIR/fnalibs
+    curl -L https://github.com/deccer/FNA-libs/archive/refs/heads/main.zip > "$DIR/fnalibs.zip"
+    if [ $? -eq 0 ]; then
+        echo "Finished downloading!"
+    else
+        >&2 echo "ERROR: Unable to download successfully."
+        exit 1
+    fi
 
-# Decompressing
-echo "Decompressing FNA libraries ..."
-mkdir -p $FNA_LIBS_DIR
-unzip $DIR/fnalibs.zip -d $FNA_LIBS_DIR
-if [ $? -eq 0 ]; then
-    echo "Finished decompressing!"
-    rm $DIR/fnalibs.zip
-else
-    >&2 echo "ERROR: Unable to decompress successfully."
-    exit 1
-fi
+    # Decompressing
+    echo "Decompressing FNA libraries ..."
+    mkdir -p $FNA_LIBS_DIR
+    unzip $DIR/fnalibs.zip -d $FNA_LIBS_DIR
+    if [ $? -eq 0 ]; then
+        echo "Finished decompressing!"
+        rm $DIR/fnalibs.zip
+    else
+        >&2 echo "ERROR: Unable to decompress successfully."
+        exit 1
+    fi
 
-FNA_LIBS_DIR=$DIR/fnalibs/FNA-libs-main
-LIB_DIR=$DIR/lib
-mkdir -p $LIB_DIR
+    FNA_LIBS_DIR=$DIR/fnalibs/FNA-libs-main
 
-# Move files to specific places...
-echo "Moving files ..."
-# FAudio
-mv $FNA_LIBS_DIR/lib64/libFAudio.so.0 $LIB_DIR/libFAudio.so
-mv $FNA_LIBS_DIR/osx/libFAudio.0.dylib $LIB_DIR/libFAudio.dylib
-mv $FNA_LIBS_DIR/x64/FAudio.dll $LIB_DIR/FAudio.dll
-# FNA3D
-mv $FNA_LIBS_DIR/osx/libMoltenVK.dylib $LIB_DIR/libMoltenVK.dylib
-mv $FNA_LIBS_DIR/osx/libvulkan.1.dylib $LIB_DIR/libvulkan.dylib
-# theorafile
-mv $FNA_LIBS_DIR/lib64/libtheorafile.so $LIB_DIR/libtheorafile.so
-mv $FNA_LIBS_DIR/osx/libtheorafile.dylib $LIB_DIR/libtheorafile.dylib
-mv $FNA_LIBS_DIR/x64/libtheorafile.dll $LIB_DIR/libtheorafile.dll
-echo "Finished moving files!"
+    # Move files to specific places...
+    echo "Moving files ..."
+    if [[ "$TARGET_BUILD_PLATFORM" == "linux" ]]; then
+        mv $FNA_LIBS_DIR/lib64/libFAudio.so.0 $LIBS_DIR/libFAudio.so
+        mv $FNA_LIBS_DIR/lib64/libtheorafile.so $LIBS_DIR/libtheorafile.so
+    elif [[ "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
+        mv $FNA_LIBS_DIR/osx/libFAudio.0.dylib $LIBS_DIR/libFAudio.dylib
+        mv $FNA_LIBS_DIR/osx/libMoltenVK.dylib $LIBS_DIR/libMoltenVK.dylib #FNA3D
+        mv $FNA_LIBS_DIR/osx/libvulkan.1.dylib $LIBS_DIR/libvulkan.dylib #FNA3D
+        mv $FNA_LIBS_DIR/osx/libtheorafile.dylib $LIBS_DIR/libtheorafile.dylib
+    elif [[ "$TARGET_BUILD_PLATFORM" == "microsoft" ]]; then
+        mv $FNA_LIBS_DIR/x64/FAudio.dll $LIBS_DIR/FAudio.dll
+        mv $FNA_LIBS_DIR/x64/libtheorafile.dll $LIBS_DIR/libtheorafile.dll
+    fi
+    echo "Finished moving files!"
 
-## Delete uncompressed folder
-rm -rf $FNA_LIBS_DIR
+    ## Delete uncompressed folder
+    rm -rf $FNA_LIBS_DIR
+}
+
+download_fna_libraries
